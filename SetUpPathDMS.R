2b0278007b07b6c597d3391e6063e76dcd7efd98
jwgibbs <mikewood@jlab.org>
2023-04-07 23:20:36 -0400

    Adding the DMS directories

--- SetUpPathDMS.R
+++ SetUpPathDMS.R
@@ -3,9 +3,9 @@
 setwd( "/Users/wood5/LDM/RMD/DMS_software/" )
 
 program.root <- getwd()
-data.root <- paste0(program.root,"/DMS Data")
-plot.root <- paste0(program.root,"/DMS Plots")
-stats.root <- paste0(program.root,"/DMS Stats")
+data.root <- paste0(program.root,"/DMS_Data")
+plot.root <- paste0(program.root,"/DMS_Plots")
+stats.root <- paste0(program.root,"/DMS_Stats")
 neutron.root <- paste0(program.root,"/NeutronAnalysis")
 
 PathIsSet <- TRUE
